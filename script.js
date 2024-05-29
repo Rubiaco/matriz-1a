0c3216046425c4aac0a87a7d351c8346fea38f97
Rubiaco <160552995+Rubiaco@users.noreply.github.com>
2024-05-29 08:54:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+const caixaPrincipal = document.querySelector(".caixa-princ;ipal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.queryselector(".texto-resutado");
+
+const pergunta1 =[
+ {  
+ enunciado: "Pergunta1",
+alternativas: 
+    "Alternativa"
+    "pergunta2"
+]
