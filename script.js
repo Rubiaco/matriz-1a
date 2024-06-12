ede3f2642cbf50f4b296567673f56ac90a6a830c
Rubiaco <160552995+Rubiaco@users.noreply.github.com>
2024-06-12 08:50:26 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -6,8 +6,18 @@ const textoResultado = document.queryselector(".texto-resutado");
 
 const pergunta1 =[
  {  
- enunciado: "Pergunta1",
-alternativas: 
-    "Alternativa"
-    "pergunta2"
+ enunciado: "Assim que saiu da escola voce se depara com umqa nova tecnologia,um
+alternativas: [
+    "Isso e assustador",
+] "Alternativa 2"
 ]
+},
+{
+enunciado:"pergunta 2"
+alternativas: [
+    "Utilizar uma ferramenta de busca na internet"
+        "Alternativas 1",
+        "Alternativa 2"
+    ]
+}
+];
