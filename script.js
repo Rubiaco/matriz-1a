410497250d5073151116e5596e28e7f67341fdfa
Rubiaco <160552995+Rubiaco@users.noreply.github.com>
2024-08-09 10:15:52 -0300

    atualizações com sugestões de melhoria

--- script.js
+++ script.js
@@ -10,17 +10,11 @@ const perguntas = [
         alternativas: [
             {
                 texto: "Isso é assustador!",
-                afirmacao: [
-                "No início ficou com medo do que essa tecnologia pode fazer. "
-                ""
-            ]
-
+                afirmacao: "No início ficou com medo do que essa tecnologia pode fazer. "
             },
             {
                 texto: "Isso é maravilhoso!",
-                afirmacao:[ "Quis saber como usar IA no seu dia a dia."
-                ""
-            ]
+                afirmacao: "Quis saber como usar IA no seu dia a dia."
             }
             
         ]
